9bfe5128d9f45cb226a24d015a2f58e0d7dee0a3
onyemakn <onyemakizito@outlook.com>
2022-03-16 11:58:05 +0100

    Updated

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+
+	{'id':1,'name':'Lets learn python'},
+	{'id':2,'name':'Design with me'},
+	{'id':3,'name':'Frontend Developers'},
+
+]
+
 
 def home(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
 	return render(request, 'room.html')
